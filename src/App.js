0b673971d4f50ee34487e9869b24ba574ba519dd
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-05-24 10:32:54 -0300

    fix navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import Sidebar from './components/Sidebar';
 import CardMetaDeVendas from './components/CardMetaDeVendas';
 import CardCalculoDeMargem from './components/CardCalculoDeMargem';
 import './App.css';
+import icone2 from './img/icone.svg';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <section className="jumbotron-sticky jumbotron px-2 pb-1 d-flex flex-row rounded-0 m-0">
+        <figure className="jumbotron-figure m-0">
+          <img src={icone2} className="img-responsive" alt="Calculadora de Teste A/B" />
+        </figure>
+        <article className="jumbotron-title m-0">
+          <h1 className="mt-2 mb-1 p-0">Orçamento do Marketing</h1>
+          <p className="p-0 m-0">Crie seu Orçamento de Marketing Digital.</p>
+        </article>
+      </section>
       <div className="container-fluid">
         <div className="row">
           <nav className="col-md-2 d-none d-md-block sidebar">
