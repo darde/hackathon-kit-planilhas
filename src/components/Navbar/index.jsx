abeff38ca65a00b16b47f53370b89fb5a3c1f2ed
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-05-24 08:23:01 -0300

    wip layout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,16 @@ import Logo from '../../img/logo.svg';
 import './styles.css';
 
 const Navbar = () => (
-  <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-    <img className="nav-bar-logo" src={Logo} alt="Logo Resultados Digitais"/>
-  </a>
+  <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+    <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+      <img className="nav-bar-logo" src={Logo} alt="Logo Resultados Digitais"/>
+    </a>
+    <ul className="navbar-nav px-3">
+      <li className="nav-item text-nowrap">
+        <a className="nav-link" href="#">Sign out</a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
