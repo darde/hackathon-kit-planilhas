e147a9e44b1d25ced46ce4426d1992f58d872f64
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-05-24 10:43:03 -0300

    fix sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,37 +3,36 @@ import Navbar from '../Navbar';
 
 const Sidebar = () => (
   <div className="sidebar-sticky">
-    <Navbar />
     <ol className="nav flex-column">
       <li className="nav-item">
         <a className="nav-link active" href="#">
-          1. Meta de vendas e crescimento esperado <span className="sr-only">(current)</span>
+          <span className="number-link-1">1. Meta de vendas e crescimento esperado</span> <span className="sr-only">(current)</span>
         </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">
-          2. Cálculo de Margem para orçamento de Marketing Digital
-                  </a>
+          <span className="number-link-2">2. Cálculo de Margem para orçamento de Marketing Digital</span>
+        </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">
-          3. Orçamento para Marketing
-                  </a>
+          <span className="number-link-3">3. Orçamento para Marketing</span>
+        </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">
-          4. Diagnóstico para Marketing
-                  </a>
+          <span className="number-link-4">4. Diagnóstico para Marketing</span>
+        </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">
-          5. Definição de necessidades, ações e projetos
-                  </a>
+          <span className="number-link-5">5. Definição de necessidades, ações e projetos</span>
+        </a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">
-          6. Orçamento detalhado de Marketing
-                  </a>
+          <span className="number-link-6">6. Orçamento detalhado de Marketing</span>
+        </a>
       </li>
     </ol>
   </div>
